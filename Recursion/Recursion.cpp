2a83d3ea43cd861043386a5d4b3fbec945327ed5
Tushar Rajaram Rao <49467516+TusharRao8334@users.noreply.github.com>
2021-08-15 09:13:29 +0530

    Recursion

---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
+
+int main()
+{
+    cout << fact(25);
+    return 0;
+}
